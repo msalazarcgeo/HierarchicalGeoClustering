849e2355de28ab69ff7ee04813978d65956056f7
Miguel <msalazar@centrogeo.edu.mx>
2021-12-01 13:36:42 -0600

    Change location and name files

--- HierarchicalGeoClustering/_nbdev.py
+++ HierarchicalGeoClustering/_nbdev.py
@@ -6,9 +6,11 @@ index = {"cluster": "00_Node_cluster.ipynb",
          "cluster_node": "00_Node_cluster.ipynb",
          "inside_polygon": "00_Node_cluster.ipynb",
          "poligon_non_convex_from_Points": "00_Node_cluster.ipynb",
-         "poligon_non_convex_random_gen": "00_Node_cluster.ipynb"}
+         "poligon_non_convex_random_gen": "00_Node_cluster.ipynb",
+         "tree_clusters": "01_tree_clusters.ipynb"}
 
-modules = ["core.py"]
+modules = ["Node_cluster.py",
+           "Tree_clusters.py"]
 
 doc_url = "https://CentroGeo.github.io/HierarchicalGeoClustering/"
 
