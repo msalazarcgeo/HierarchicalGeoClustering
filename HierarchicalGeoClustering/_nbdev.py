61ccf89a604475dbaa3d30cfdbc90062159551e4
Miguel <msalazar@centrogeo.edu.mx>
2021-12-01 19:09:56 -0600

    Clustering module added

--- HierarchicalGeoClustering/_nbdev.py
+++ HierarchicalGeoClustering/_nbdev.py
@@ -7,11 +7,12 @@ index = {"cluster": "00_Node_cluster.ipynb",
          "inside_polygon": "00_Node_cluster.ipynb",
          "poligon_non_convex_from_Points": "00_Node_cluster.ipynb",
          "poligon_non_convex_random_gen": "00_Node_cluster.ipynb",
-         "module_path": "01_tree_clusters.ipynb",
+         "module_path": "02_Clustering.ipynb",
          "tree_clusters": "01_tree_clusters.ipynb"}
 
 modules = ["Node_cluster.py",
-           "Tree_clusters.py"]
+           "Tree_clusters.py",
+           "Clustering.py"]
 
 doc_url = "https://CentroGeo.github.io/HierarchicalGeoClustering/"
 
